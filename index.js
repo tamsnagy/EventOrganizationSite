fc5ea94777c45d1c0540ede352ee577618f3ed5f
Tamás Nagy <ntamas13@gmail.com>
2016-03-27 21:13:18 +0200

    Edit and device list views are separated according to teachers comment.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('static'));
+app.use('/static', express.static('static'));
 
 var server = app.listen(3000, function () {
 });
