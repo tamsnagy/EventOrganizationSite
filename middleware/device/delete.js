57e79748c402eea2fcaa6869d367fb365fef83ef
Tamás Nagy <ntamas13@gmail.com>
2016-05-27 19:53:40 +0200

    device delete will delete orders where the device was ordered too.

--- middleware/device/delete.js
+++ middleware/device/delete.js
@@ -6,14 +6,27 @@ var requireOption = require('../common').requireOption;
 module.exports = function (objectrepository) {
 
     var deviceModel = requireOption(objectrepository, 'deviceModel');
+    var orderModel = requireOption(objectrepository, 'orderModel');
 
     return function (req, res, next) {
 
         deviceModel.findByIdAndRemove(req.query.id, function (err) {
             if(err) {
-                return next(new Error('Error getting devices'));
+                return next(new Error('Error deleting devices'));
             }
-            res.redirect('/device/list');
+            orderModel.find({'_device':req.query.id}, function (err, orders) {
+                if(err) {
+                    return next(new Error('Error deleting orders'));
+                }
+                for(var i = 0; i < orders.length; i++) {
+                    orderModel.findByIdAndRemove(orders[i]._id, function (err) {
+                        if (err) {
+                            return next(new Error('Error deleting orders'));
+                        }
+                    });
+                }
+                res.redirect('/device/list');
+            });
         });
     };
 
